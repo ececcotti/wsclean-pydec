a35ebebcd75a7f0531bea5cd54726684226a922d
Emilio Ceccotti <71387028+ececcotti@users.noreply.github.com>
2020-10-19 00:42:05 +0200

    Delete pydec.sh (main directory)

--- pydec.sh
+++
@@ -1,1 +0,0 @@
-wsclean -size 1024 1024 -scale 0.03asec -save-first-residual -channels-out 1 -pol xx -niter 10000 -auto-threshold 3 -mgain 0.8 -python-deconvolution deconvolution-example.py -name L692158_SB109_pydec ../L692158/L692158_SB109_uv.MS
