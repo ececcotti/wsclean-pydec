cb05bdd2fcd426b3b1d1f2f2364ad0653b8f7477
Emilio Ceccotti <emilio@node100.intra.dawn.rug.nl>
2020-10-23 17:30:59 +0200

    Add label cmap option in plot_fits.py

--- python_scripts/plot_fits.py
+++ python_scripts/plot_fits.py
@@ -11,9 +11,10 @@ o.add_option('--size', dest='size', default="8,6", type="str", help='Size of the
 o.add_option('--vmin', dest='vmin', default=None, type="float", help='Minimum value of the colormap (default: data minimum)')
 o.add_option('--vmax', dest='vmax', default=None, type="float", help='Maximum value of the colormap (default: data maximum)')
 o.add_option('--cmap', dest='cmap', default="jet", type="str", help='Colormap (default: jet)')
+o.add_option('--lbl-cmap', dest='lbl-cmap', default="Jy/beam", type="str", help='Label of the color map (default: Jy/beam)')
 o.add_option('--pdf', dest='save_pdf', default=False, action='store_true', help='Save image as pdf')
 o.add_option('--png', dest='save_png', default=False, action='store_true', help='Save image as png')
-o.add_option('--name', dest='outfile', default=None, type="str", help='Output image name with no extension (default = fits file name')
+o.add_option('--name', dest='outfile', default=None, type="str", help='Output image name with no extension (default: fits name')
 opts,args = o.parse_args(sys.argv[1:])
 
 fitsname = args[0]
@@ -38,7 +39,7 @@ if vmax0 == None: vmax0 = np.max(data)
 plt.imshow(data, origin='lower', cmap=opts.cmap,
 	aspect='auto', interpolation='none', 
 	vmin=vmin0, vmax=vmax0)
-plt.colorbar().set_label('Jy/beam', rotation=90)
+plt.colorbar().set_label(opts.lbl-cmap, rotation=90)
 ax.coords.grid(color='silver', ls='solid', alpha=0.5)
 plt.xlabel('Right Ascension (J2000)')
 plt.ylabel('Declination (J2000)')
@@ -50,7 +51,7 @@ plt.title(outimage)
 if opts.save_pdf:
 	plt.savefig(outimage + '.pdf')
 if opts.save_png:
-	plt.savefig(outimage + 'png', dpi=200)
+	plt.savefig(outimage + '.png', dpi=200)
 plt.show()
 plt.close()
 
